0c514c4cebed16326216eef8a901f5f30eadf173
HannahLihui <3303594461@qq.com>
2019-02-15 14:20:01 +0800

    提交

--- src/main/java/cn/tycoding/controller/admin/UploadFileConfig.java
+++ src/main/java/cn/tycoding/controller/admin/UploadFileConfig.java
@@ -9,13 +9,4 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-@Configuration
-public class UploadFileConfig extends WebMvcConfigurationSupport{
 
-	  @Override
-	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	        registry.addResourceHandler("/upload/**").addResourceLocations("file:C:/Users/Administrator/AppData/Local/Temp/tomcat-docbase.8476537028754118257.8080/upload/");
-	        super.addResourceHandlers(registry);
-	    }
-
-}
